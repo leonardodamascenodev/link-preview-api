bf3781028a44f20357976af8ea03cc20276d3b65
Leonardo Correia Damasceno <leonardo.c.damasceno@edu.senai.br>
2026-06-29 13:15:17 -0300

    fix: corrigindo codigos

--- index.js
+++ index.js
@@ -43,5 +43,3 @@ app.get('/api/preview', async (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-app.use(express.static(path.join(__dirname, 'public')));
